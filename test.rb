431ff595c7eb67912eda7c9d00a0c8ad60c3508e
khoi_nguyen_deepstack <khoi.nguyen@deepstack.io>
2023-04-10 11:38:19 -0700

    Test file... ignore

--- test.rb
+++ test.rb
@@ -1,15 +1,27 @@
-# Dir.glob('./active_merchant/**/*.rb')
+
 require 'activemerchant'
 
-@gateway = ActiveMerchant::Billing::Deepstack.new(
-    :client_id => '123',
-    :api_username => '123',
-    :api_password => '123'
+gateway = ActiveMerchant::Billing::DeepstackGateway.new(
+    :client_id => '1003',
+    :api_username => 'GPF1e74ujhy',
+    :api_password => 'GPikuyujhy'
 )
-puts("Hello world")
 
-# gateway = ActiveMerchant::Billing::GloballyPaidGateway.new(
-#     :publishable_api_key => 'T0FL5VDNQRK0V6H1Z6S9H2WRP8VKIVWO', 
-#     :app_id => '6652820b-6a7a-4d36-bc32-786e49da1cbd', 
-#     :shared_secret => 'ME1uVox0hrk7i87e7kbvnID38aC2U3X8umPH0D+BsVA=', 
-#     :sandbox => true)
+creditCard = ActiveMerchant::Billing::CreditCard.new(
+    :first_name => "John",
+    :last_name => "Doe",
+    :number => "4120469701788378",
+    :month => "01",
+    :year => "27",
+    :verification_value => "999"
+)
+
+options = {
+    :card_billing_address => "123 Main St",
+    :card_billing_zipcode => '12345',
+}
+
+response = gateway.getToken(creditCard, options)
+puts response.params
+
+# puts("Hello world")
